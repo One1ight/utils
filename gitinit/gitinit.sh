0c5516dc471726a30174fdff7762f9d61b3b9579
one1ight <one_1ight@hotmail.com>
2020-04-29 09:24:17 +0800

    add comment

--- gitinit/gitinit.sh
+++ gitinit/gitinit.sh
@@ -5,15 +5,18 @@ set -o pipefail
 
 owner=''
 name=''
+# help print info and usage
 function help() {
 	echo "gitinit is a little script to help you create a new repository on the command line"
-	echo "But first you need to create a remote bare respository and leave the rest to gitinit"
+	echo "But first you need to create a remote bare repository and leave the rest to gitinit"
 	usage
 	exit 1
 }
+# usage print usage
 function usage(){
 	printf "script usage:\n\t[-v] version\n \t[-o owner] set owner\n\t[-n repositoryName] set repository name\n"
 }
+# get options( owner, repository name.)
 while getopts 'vho:n:' OPTION; do
 	case "$OPTION" in
 		v)
@@ -37,18 +40,24 @@ while getopts 'vho:n:' OPTION; do
 	esac
 done
 shift "$(($OPTIND -1))"
+# set owner if not exist
 if [ -z $owner ] ; then 
 	owner=$(whoami)	
 fi
+# set repository name if not exist
 if [ -z $name ] ; then
 	name=${PWD##*/}
 fi
+# print owner and repository
 echo "The owner will be :$owner."
-echo "The respositoryName will be :$name."
+echo "The repositoryName will be :$name."
 
+# continue or exit
 echo -n "Are you sure?(y/n)"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ]; then
+
+	# execute git command
 	echo "*******************README.md***********************"
 	echo "#$name" >> README.md
 	echo "*******************git init************************"
